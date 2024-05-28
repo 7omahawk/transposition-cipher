a7ecab7ae881f933647754bb4890dc7741a0811f
7omahawk <ahulr.official@gmail.com>
2024-05-28 16:15:56 +0600

    updated

--- transpositionCipher.py
+++ transpositionCipher.py
@@ -3,14 +3,15 @@
 import sys
 import math
 
+
 def encryption(key, userInput):
-    userInput = userInput.replace(" ","")    # excluding space from the sentence
-    sizeOfKey = len(str(key))   # column size
+    userInput = userInput.replace(" ", "")  # excluding space from the sentence
+    sizeOfKey = len(str(key))  # column size
     sizeOfInput = len(userInput)
-    global inputSize   # this is the global variable
+    global inputSize  # this is the global variable
     inputSize = sizeOfInput
-    
-    numberOfRow = math.ceil(sizeOfInput / sizeOfKey) # row size
+
+    numberOfRow = math.ceil(sizeOfInput / sizeOfKey)  # row size
     totalLengthOfInput = sizeOfKey * numberOfRow
 
     # for padding
@@ -19,71 +20,89 @@ def encryption(key, userInput):
         for i in range(padding):
             userInput = userInput + "z"
 
-    matrix = [["" for space in range(sizeOfKey)] for space in range(numberOfRow)] # making matrix
+    matrix = [
+        ["" for space in range(sizeOfKey)] for space in range(numberOfRow)
+    ]  # making matrix
 
     # row by row write-------------------###[step 1]
     # inserting every single value of input into matrix with padding
     for i in range(numberOfRow):
         for j in range(sizeOfKey):
-            index = i * sizeOfKey + j   # when i = 0,1 and j = 0,1 then index =[[0][0],[0][1]],[[1][0],[1][1]] 
+            index = (
+                i * sizeOfKey + j
+            )  # when i = 0,1 and j = 0,1 then index =[[0][0],[0][1]],[[1][0],[1][1]]
             if index < len(userInput):
                 matrix[i][j] = userInput[index]
 
     # shuffle the matrix with the key-------------------###[step 2]
-    strKey = str(key) # making integer key to string for store every individual index
-    shuffledMatrix = [["" for space in range(sizeOfKey)] for space in range(numberOfRow)] # making  new matrix for store the shuffled value
+    strKey = str(key)  # making integer key to string for store every individual index
+    shuffledMatrix = [
+        ["" for space in range(sizeOfKey)] for space in range(numberOfRow)
+    ]  # making  new matrix for store the shuffled value
 
     for i in range(numberOfRow):
         for j in range(sizeOfKey):
-            shuffledMatrix[i][j] = matrix[i][int(strKey[j])-1]
+            shuffledMatrix[i][j] = matrix[i][int(strKey[j]) - 1]
 
     # count by column-------------------###[step 3]
     cipher = ""
-    for i in range(sizeOfKey):     # have to reapet as column size
-        for j in range(numberOfRow):    # have to reapet as row size
-            value = shuffledMatrix[j][i]   # when i = 0,1 and j = 0,1 then value = [[0][0],[1][0]], [[0][1],[1][1]]
+    for i in range(sizeOfKey):  # have to repeat as column size
+        for j in range(numberOfRow):  # have to repeat as row size
+            value = shuffledMatrix[j][
+                i
+            ]  # when i = 0,1 and j = 0,1 then value = [[0][0],[1][0]], [[0][1],[1][1]]
             cipher = cipher + value
     print(f"The encrypted message is: {cipher}")
 
 
 def decryption(userInput, key):
 
-    sizeOfKey = len(str(key))   # column size
+    sizeOfKey = len(str(key))  # column size
     sizeOfInput = len(userInput)
-    numberOfRow = math.ceil(sizeOfInput / sizeOfKey) # row size
+    numberOfRow = math.ceil(sizeOfInput / sizeOfKey)  # row size
 
-    matrix = [["" for space in range(sizeOfKey)] for space in range(numberOfRow)] # making matrix
+    matrix = [
+        ["" for space in range(sizeOfKey)] for space in range(numberOfRow)
+    ]  # making matrix
     # column by column write-------------------###[step 1]
     for i in range(sizeOfKey):
         for j in range(numberOfRow):
-            index = i * numberOfRow + j   # when i = 0,1 and j = 0,1 then index =[[0][0],[0][1]],[[1][0],[1][1]] 
+            index = (
+                i * numberOfRow + j
+            )  # when i = 0,1 and j = 0,1 then index =[[0][0],[0][1]],[[1][0],[1][1]]
             if index < len(userInput):
-                matrix[j][i] = userInput[index]   # when i = 0,1 and j = 0,1 then value of matrix= [[0][0],[1][0]], [[0][1],[1][1]]
+                matrix[j][i] = userInput[
+                    index
+                ]  # when i = 0,1 and j = 0,1 then value of matrix= [[0][0],[1][0]], [[0][1],[1][1]]
 
     # shuffle by key-------------------###[step 2]
-    strKey = str(key) # making integer key to string for store every individual index
-    shuffledMatrix = [["" for space in range(sizeOfKey)] for space in range(numberOfRow)] # making  new matrix for store the shuffled value
+    strKey = str(key)  # making integer key to string for store every individual index
+    shuffledMatrix = [
+        ["" for space in range(sizeOfKey)] for space in range(numberOfRow)
+    ]  # making  new matrix for store the shuffled value
 
     for i in range(numberOfRow):
         for j in range(sizeOfKey):
-            shuffledMatrix[i][int(strKey[j])-1] = matrix[i][j]
+            shuffledMatrix[i][int(strKey[j]) - 1] = matrix[i][j]
 
     # write the plaintext-------------------###[step 3]
     plaintext = ""
-    for i in range(numberOfRow):     # have to reapet as row size
-        for j in range(sizeOfKey):    # have to reapet as column size
-            value = shuffledMatrix[i][j]   # when i = 0,1 and j = 0,1 then value = [[0][0],[0][1]], [[1][0],[1][1]]
+    for i in range(numberOfRow):  # have to repeat as row size
+        for j in range(sizeOfKey):  # have to repeat as column size
+            value = shuffledMatrix[i][
+                j
+            ]  # when i = 0,1 and j = 0,1 then value = [[0][0],[0][1]], [[1][0],[1][1]]
             plaintext = plaintext + value
 
     # extract the maintext
     finalText = ""
-    for i in range(len(plaintext)): 
-        if i < inputSize:    # input size is a global variable
+    for i in range(len(plaintext)):
+        if i < inputSize:  # input size is a global variable
             finalText = finalText + plaintext[i]
     print(f"The original decrypted message is: {finalText}")
 
 
-while(True):
+while True:
     print("Enter your choice(Number): ")
     print("1. Encryption: ")
     print("2. Decryption: ")
@@ -94,12 +113,12 @@ while(True):
     def choice(number):
         if number == 1:
             userInput = input("Enter your text to encrypt: ")
-            key = int(input("Enter the key: "))
+            key = int(input("Enter the key(ex. 24315): "))
             userInput = userInput.lower()
             encryption(key, userInput)
         elif number == 2:
             userInput = input("Enter your text to decrypt: ")
-            key = int(input("Enter the key: "))
+            key = int(input("Enter the key(ex. 24315): "))
             userInput = userInput.lower()
             decryption(userInput, key)
         elif number == 3:
@@ -107,4 +126,4 @@ while(True):
         else:
             print("Input should be a number from 1 to 3")
 
-    choice(number) 
+    choice(number)
